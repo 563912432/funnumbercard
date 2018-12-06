a21c004ad299ddd575de4550e8f047baa56d49d1
陈帅 <1301491593@qq.com>
2018-12-06 11:34:28 +0800

    '123'

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import list from '@/components/list'
 import detail from '@/components/detail'
+import introduce from '@/components/introduce'
+import adDetail from '@/components/adDetail'
 
 Vue.use(Router)
 
@@ -23,5 +25,15 @@ export default new Router({
       name: 'd',
       component: detail
     },
+    {
+      path: '/introduce/:url',
+      name: '/introduce',
+      component: introduce
+    },
+    {
+      path: '/adDetail/:type',
+      name: '/adDetail',
+      component: adDetail
+    }
   ]
 })
